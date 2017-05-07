91f8c6a6327b3e8f6f9044fea59b32d6fa333258
Peter B Smith <peterbutlersmith@gmail.com>
2017-05-07 13:52:50 -0400

    add: website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    let randomNumber = this.getRandomNumber();
+    Observable.interval(randomNumber)
+      .subscribe((i) => {
+        randomNumber = this.getRandomNumber();
+        this.oxymoron = this.oxymorons[i % this.oxymorons.length]
+      });
+  }
+
+  oxymorons = [
+    'Rough Gentleman',
+    'Moral Scoundrel',
+    'Outgoing Introversion',
+    'Ignorant Intellect',
+    'Largely Minimal',
+    'Scatter Precision',
+    'Secluded Extraversion'
+  ]
+
+  oxymoron = ''
+
+  getRandomNumber() {
+    return 750 * (1 + Math.random());
+  }
+
 }
