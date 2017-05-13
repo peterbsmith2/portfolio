01199dab64785124ceff89bbe3e8a641700b65fd
Peter B Smith <peterbutlersmith@gmail.com>
2017-05-13 17:16:13 -0400

    add: arrows to homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/fromEvent';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +21,10 @@ export class AppComponent implements OnInit {
         randomNumber = this.getRandomNumber();
         this.oxymoron = this.oxymorons[i % this.oxymorons.length]
       });
+
+    Observable.fromEvent(document, 'keydown')
+    .filter((e: KeyboardEvent) =>  e.keyCode <= 40 && e.keyCode >= 37 )
+    .subscribe((e: KeyboardEvent)=> console.log(e.keyCode));
   }
 
   oxymorons = [
