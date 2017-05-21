f2f63a6b25afef25e5aeb97f3aeb8f51717d0941
Peter B Smith <peterbutlersmith@gmail.com>
2017-05-21 12:47:56 -0400

    add animations and new oxymorons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
@@ -11,7 +18,20 @@ import 'rxjs/add/observable/fromEvent';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('animationState', [
+      state('inactive', style({
+        transform: 'scale(1)'
+      })),
+      state('active',   style({
+        background: 'linear-gradient(rgba(20,20,20, .5),rgba(20,20,20, .5))',
+        transform: 'scale(1.1)'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
 
@@ -26,26 +46,27 @@ export class AppComponent implements OnInit {
       });
 
     Observable.fromEvent(document, 'keydown')
-      .filter((e: KeyboardEvent) =>  e.keyCode <= 40 && e.keyCode >= 37 )
+      .filter((e: KeyboardEvent) => e.keyCode <= 40 && e.keyCode >= 37 )
       .map((e: KeyboardEvent)=>{
-        this.arrows[e.key.toLowerCase()] = true;
+        this.arrows[e.key.toLowerCase()] = "active";
         return e;
       })
       .delay(150)
       .subscribe((e: KeyboardEvent)=>{
-        this.arrows[e.key.toLowerCase()] = false;
+        this.arrows[e.key.toLowerCase()] = "inactive";
       });
   }
 
   arrows = {
-    arrowup: false,
-    arrowright: false,
-    arrowdown: false,
-    arrowleft: false
+    arrowup: "inactive",
+    arrowright: "inactive",
+    arrowdown: "inactive",
+    arrowleft: "inactive"
   }
 
 
   oxymorons = [
+    'Dimly Shrewd',
     'Unknowingly Self-Aware',
     'Moral Scoundrel',
     'Outgoing Introversion',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -14,6 +15,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     AppRoutingModule
