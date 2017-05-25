cc3bea184d84a68e6226153de4d2bb612c0a218e
Peter B Smith <peterbutlersmith@gmail.com>
2017-05-25 10:27:04 -0400

    add: angular material input, password input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MdInputModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PasswordComponent } from './password/password.component';
+
+
 
 @NgModule({
   declarations: [
@@ -20,7 +23,8 @@ import { PasswordComponent } from './password/password.component';
     BrowserAnimationsModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdInputModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
