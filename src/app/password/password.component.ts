05ddbaafa1fa35c98124fd9e0eade564e6df5c04
Peter B Smith <peterbutlersmith@gmail.com>
2017-06-01 14:11:11 -0400

    remove: flip animation from password

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../router.animations';
-import { flipAnimation } from './password.animations';
 
 @Component({
   selector: 'app-password',
   templateUrl: './password.component.html',
   styleUrls: ['./password.component.css'],
-  animations: [routerTransition(), flipAnimation()],
+  animations: [routerTransition()],
   host: {'[@routerTransition]': ''}
 })
 export class PasswordComponent implements OnInit {
 
   constructor() { }
 
-  flipped: Boolean = false;
+  visible: Boolean = false;
 
-  flip(password: string): void {    
+  show(password: string): void {    
     if (password === "swordfish") {
-      this.flipped = !this.flipped;
+      this.visible = !this.visible;
     }
   }
 
